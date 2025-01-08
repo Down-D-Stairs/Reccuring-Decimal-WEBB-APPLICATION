e012d3a0a5ea149ac715aa136a6377160caa07b2
kaushik.karumudi@gmail.com <Down-D-Stairs>
2025-01-08 10:51:08 -0700

    Initial commit

--- RD-WebAPP/microsoft-auth-app/client/src/App.js
+++ RD-WebAPP/microsoft-auth-app/client/src/App.js
@@ -6,7 +6,7 @@ const msalConfig = {
   auth: {
     clientId: "4f97959d-fbe9-4911-9f1b-e425218d5fb4",
     authority: "https://login.microsoftonline.com/955ed657-dd5f-490b-8a55-5abaa73481f2",
-    redirectUri: "http://localhost:3000",
+    redirectUri: "https://reccuring-decimal-webb-application.onrender.com",
   }
 };
 
