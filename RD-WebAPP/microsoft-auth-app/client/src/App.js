6db2a46b0f1b1d362ca1e52ac92247f5c5a0197c
kaushik.karumudi@gmail.com <Down-D-Stairs>
2025-06-16 10:13:33 -0700

    Upload full Angular project structure

--- RD-WebAPP/microsoft-auth-app/client/src/App.js
+++ RD-WebAPP/microsoft-auth-app/client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import * as msal from "@azure/msal-browser";
 import ExpenseManager from './Components/ExpenseManager';
 import TimeTableManager from './Components/TimeTableManager';
